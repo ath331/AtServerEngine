903dda833a993167520a9c766abceff0a501bc53
ath331 <kstnftk@naver.com>
2024-09-09 23:46:35 +0900

    로그 출력 함수 단순화

--- GameServer/Logic/Utils/Log/AtLog.h
+++ GameServer/Logic/Utils/Log/AtLog.h
@@ -7,7 +7,8 @@
 
 
 #define INFO_LOG( msg )    AtLog::PrintMsg( __FILE__, __LINE__, msg );
-#define WARNNING_LOG( msg ) AtLog::PrintRedMsg( __FILE__, __LINE__, msg );
+#define INFO_LOG_GREEN( msg )    AtLog::PrintMsg( __FILE__, __LINE__, msg, AtStringColor::EColor::Green );
+#define WARNNING_LOG( msg ) AtLog::PrintMsg( __FILE__, __LINE__, msg, AtStringColor::EColor::Red );
 
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -18,8 +19,9 @@ class AtStringColor
 public:
 	enum class EColor
 	{
-		None = 0, // 기본값
-		Red  = 1, // 빨간색
+		None  = 0, // 기본값
+		Red   = 1, // 빨간색
+		Green = 2, // 초록색
 	};
 			
 public:
@@ -32,8 +34,11 @@ public:
 			return;
 
 		m_handle = GetStdHandle( STD_OUTPUT_HANDLE );
+
 		if ( m_color == EColor::Red )
 			SetConsoleTextAttribute( m_handle, FOREGROUND_RED );
+		else if ( m_color == EColor::Green )
+			SetConsoleTextAttribute( m_handle, FOREGROUND_GREEN );
 	}
 
 	// 소멸자
@@ -58,10 +63,11 @@ class AtLog
 {
 public:
 	// msg를 출력한다.
-	static AtVoid PrintMsg( const char* file, AtInt32 line, AtString msg );
-
-	// msg를 빨간색으로 출력한다.
-	static AtVoid PrintRedMsg( const char* file, AtInt32 line, AtString msg );
+	static AtVoid PrintMsg(
+		const char*                 file,
+		      AtInt32               line,
+		      AtString              msg,
+		      AtStringColor::EColor color = AtStringColor::EColor::None );
 
 private:
 	// 파일 경로에서 파일 이름만 추출하는 함수
